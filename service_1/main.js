46acfd24ce090e78651995a998d8275f0a4d86c3
rdecamps <renan.decamps@gmail.com>
2015-01-27 22:04:00 -0500

    je sais pas quoi dire alors voilà

--- service_1/main.js
+++ service_1/main.js
@@ -1,3 +1,29 @@
+var express = require('express'),
+	/* routes  = require('routes'), */
+	app     = express();
+
+
+app.get('/', function(req, res) {
+	
+	var test = {
+		prop1 : "test",
+		prop2 : 123,
+		prop3 : {
+			test : [ 1, 2, 3]
+		}
+	};
+
+	res.send(JSON.stringify(test));
+});
+
+var server = app.listen(3000, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('App listening at http://%s:%s', host, port);
+});
+
+/*
 var Graphe = require('Graphe');
 
 var graphe = new Graphe('graphe.json');
@@ -6,3 +32,4 @@ console.log(graphe.toString());
 console.log("----------------------");
 
 console.log(graphe.toJSON());
+*/
