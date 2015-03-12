903407ad41057e9ebd5d8946f37a2a306ec7b80f
Renan Decamps <renan.decamps@gmail.com>
2015-03-12 10:50:21 -0500

    MAJ du gros bordel :skull:

---
+++ service_1/main.js
@@ -0,0 +1,14 @@
+// On ajoute les dépendances
+var express = require('express'),
+	Graphe  = require('Graphe');
+
+// On rend les objets principaux
+// accessibles globalement
+global.app    = express();
+global.Graphe = Graphe;
+
+// Initialisation des routes
+require('routes');
+
+// Initialisation du serveur et lancement
+require('init');
