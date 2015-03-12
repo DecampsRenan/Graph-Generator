8273177b81d6836a327005c9de5424118e5c41c5
RoadFen <roadfen@gmail.com>
2015-03-12 15:07:33 -0400

    com

--- portail/public/js/code.js
+++ portail/public/js/code.js
@@ -1,109 +1,110 @@
 $(function(){ // on dom ready
 
-    var graph = null;
+	var graph = null;
 
-    function updateGraph(graph) {
-        var cy = cytoscape({
-            container: document.getElementById('cy'),
+	function updateGraph(graph) {
+		var cy = cytoscape({
+			container: document.getElementById('cy'),
 
-            style: cytoscape.stylesheet()
-            .selector('node')
-            .css({
-                'content': 'data(id)'
-            })
-            .selector('edge')
-            .css({
-                'target-arrow-shape': '', //forme du trait triangle pour avoir une fleche
-                'width': 4,
-                'line-color': '#ddd',
-                'target-arrow-color': '#ddd'
-            })
-            .selector('.highlighted')
-            .css({
-                'background-color': '#61bffc',
-                'line-color': '#61bffc',
-                'target-arrow-color': '#61bffc',
-                'transition-property': 'background-color, line-color, target-arrow-color',
-                'transition-duration': '0.5s'
-            }),
+			style: cytoscape.stylesheet()
+			.selector('node')
+			.css({
+				'content': 'data(id)'
+			})
+			.selector('edge')
+			.css({
+				'target-arrow-shape': '', //forme du trait triangle pour avoir une fleche
+				'width': 4,
+				'line-color': '#ddd',
+				'target-arrow-color': '#ddd'
+			})
+			.selector('.highlighted')
+			.css({
+				'background-color': '#61bffc',
+				'line-color': '#61bffc',
+				'target-arrow-color': '#61bffc',
+				'transition-property': 'background-color, line-color, target-arrow-color',
+				'transition-duration': '0.5s'
+			}),
 
-            elements: graph,
+			elements: graph,
 
-            layout: {
-              name: 'circle',
+			layout: {
+				name: 'circle',
 
-              fit: true, // whether to fit to viewport
-              padding: 30, // fit padding
-              boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
-              animate: true, // whether to transition the node positions
-              animationDuration: 500, // duration of animation in ms if enabled
-              ready: undefined, // callback on layoutready
-              stop: undefined // callback on layoutstop
-            }
-        });
+				fit: true, // whether to fit to viewport
+				padding: 30, // fit padding
+				boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+				animate: true, // whether to transition the node positions
+				animationDuration: 500, // duration of animation in ms if enabled
+				ready: undefined, // callback on layoutready
+				stop: undefined // callback on layoutstop
+			}
+		});
 
-        var bfs = cy.elements().bfs('#a', function(){}, true);
-    }
+		var bfs = cy.elements().bfs('#a', function(){}, true);
+	}
 
-    $("button").nextAll('button').prop('disabled', 'true');
+	$("button").nextAll('button').prop('disabled', 'true');
 
-    // Récupération du graphe généré par le service 1
-    $( "#generation" ).click(function(event) {
-        event.preventDefault();
-        var nbSommet = $("#nbSommet").val(),
-            densite  = $("#densite").val();
+	// Récupération du graphe généré par le service 1
+	$( "#generation" ).click(function(event) {
+		event.preventDefault();
+		var nbSommet = $("#nbSommet").val(),
+			densite  = $("#densite").val();
 
-        $.ajax({
-            url: 'http://localhost:8081/create/' + nbSommet + '/' + densite,
-            dataType: 'json',
-            crossDomain: true,
-            success : function(obj, statut){
-                console.log("Reçu :", obj);
-                graph = obj;
-                updateGraph(obj);
-                $("button").removeProp('disabled');
-            },
-            error : function(req, statut, err) {
-                console.warn(err);
-            }
-        });
+		$.ajax({
+			url: 'http://localhost:8081/create/' + nbSommet + '/' + densite,
+			dataType: 'json',
+			crossDomain: true,
+			success : function(obj, statut){
+				console.log("Reçu :", obj);
+				graph = obj;
+				updateGraph(obj);
+				$("button").removeProp('disabled');
+			},
+			error : function(req, statut, err) {
+				console.warn(err);
+			}
+		});
 
-        return false;
-    });
-    
-    
+		return false;
+	});
 
-    // Envoi et récupération du chemin le plus court
-    // géré par le service 2
-<<<<<<< HEAD
-    //$( '#djikstra' )
-    
-    // Envoies du graphe et récupérations des graphes couvrant minimals
-    var socket = io.connect('http://localhost:8083');
-	
-	$("#kruskal").click(function(){
-    	socket.emit('graphe', 'test');
-		alert('envoie');
-=======
-    $( '#djikstra' ).click(function(event) {
-        event.preventDefault();
 
-        $.ajax({
-            url: 'http://localhost:8082/create',
-            data: {'graphe': graph},
-            method: 'POST',
-            dataType: 'json',
-            crossDomain: true,
-            success: function(obj, statut) {
-                console.log('Reçu du service 2:', obj);
-            },
-            error: function(req, statut, err) {
-                console.warn(err);
-            }
-        });
 
-        return false;
->>>>>>> origin/master
-    });
+	// Envoi et récupération du chemin le plus court
+	// géré par le service 2
 
-}); // on dom ready
+	// Envoies du graphe et récupérations des graphes couvrant minimals
+	var socket = io.connect('http://localhost:8083');
+
+	$("#kruskal").click(function(event){
+		event.preventDefault();
+		socket.emit('graphe', graph);
+		console.log("test");
+		return false;
+	});
+
+	$( '#djikstra' ).click(function(event) {
+		event.preventDefault();
+
+		$.ajax({
+			url: 'http://localhost:8082/create',
+			data: {'graphe': graph},
+			method: 'POST',
+			dataType: 'json',
+			crossDomain: true,
+			success: function(obj, statut) {
+				console.log('Reçu du service 2:', obj);
+			},
+			error: function(req, statut, err) {
+				console.warn(err);
+			}
+		});
+
+		return false;
+		
+	});
+
+}); // on dom ready	
--- service_3/main.js
+++ service_3/main.js
@@ -3,14 +3,52 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 
-
 io.on('connection', function(socket){
-  console.log('a user connected');
+	console.log('a user connected');
+	
+	socket.on('graphe', function(socket){
+		console.log('reception du beau graphe <3');
+		console.log(socket);
+		var LeGraphe = socket;
+		//console.log(JSON.stringify(LeGraphe));
+
+		var krustalResultat = kruskal(LeGraphe);
+	});
+
 });
 
-io.on('graphe', function(socket){
-  console.log('a user graphe');
-});
+function kruskal(unGraphe){
+	// tableau de sommets
+	var T = [];
+	console.log(unGraphe.nodes.length);
+	for(i=0; i<unGraphe.nodes.length; i++){
+		console.log(unGraphe.nodes[i].data);
+	}
+	var tabEdges = [];
+
+	
+	// TRI DES ARRETES DU MOINS FORT AU PLUS FORT
+	var n = unGraphe.edges.length;
+	var tabEdges = unGraphe.edges;
+	var tabFinal = null;
+	for(i=1; i<n; i++){
+		j=i;
+		v=tabEdges[i];
+		while(j>0 && v.data.weight < tabEdges[j-1].data.weight){
+			tabEdges[j] = tabEdges[j-1];
+			j = j-1;
+		}
+		tabEdges[j] = v;
+	}
+	
+	for(a=0; a<n; a++){
+		if(	
+	}
+
+	console.log("-----");
+	console.log(tabEdges);
+	return null;
+}
 
 
 
