038426de14a74d71ddda5b81e4caf663b02a6bfe
Quentin BRODIER <quentin.brodier@gmail.com>
2015-03-13 14:41:56 -0400

    Algo Kruskal
    Algo Kruskal

--- service_3/main.js
+++ service_3/main.js
@@ -7,7 +7,8 @@ io.on('connection', function(socket){
 	console.log('a user connected');
 	
 	socket.on('graphe', function(socket){
-		console.log('reception du beau graphe <3');
+		console.log('\n1 /================================================');
+		console.log('GRAPHE D’ENTREE\n');
 		console.log(socket);
 		var LeGraphe = socket;
 		//console.log(JSON.stringify(LeGraphe));
@@ -17,19 +18,42 @@ io.on('connection', function(socket){
 
 });
 
+function contains(a, obj) {
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function kruskal(unGraphe){
-	// tableau de sommets
-	var T = [];
+	
+	// TABLEAU DE SOMMETS
+	console.log('\n2 /================================================');
+	console.log('NOMBRE DE NOEUDS & Affichage\n');
 	console.log(unGraphe.nodes.length);
-	for(i=0; i<unGraphe.nodes.length; i++){
-		console.log(unGraphe.nodes[i].data);
-	}
-	var tabEdges = [];
 
 	
+	var T = new Array();
+
+
+	for(i=0; i<unGraphe.nodes.length; i++){
+		console.log(unGraphe.nodes[i].data);
+		T[i] = new Array();
+		T[i][0] = unGraphe.nodes[i].data.id;
+	}
+	var tabEdges = [];
+	
+
 	// TRI DES ARRETES DU MOINS FORT AU PLUS FORT
 	var n = unGraphe.edges.length;
 	var tabEdges = unGraphe.edges;
+
+	console.log('\n3 /================================================');
+	console.log('TABEDGES AVANT TRI\n');
+	console.log(tabEdges);
+
 	var tabFinal = null;
 	for(i=1; i<n; i++){
 		j=i;
@@ -41,12 +65,68 @@ function kruskal(unGraphe){
 		tabEdges[j] = v;
 	}
 	
-	for(a=0; a<n; a++){
+	console.log('\n4 /================================================');
+	console.log('TABEDGES APRES TRI\n');
+	console.log(tabEdges);
+
+	
+
+	// ALGORITHME DE KRUSKAL - ON GARDE LE MINIMUM D’ARRETE
+	// TOUT EN LIANT LE GRAPHE
+
+	var tabEdgesKruskal = []; // Tableau final des arêtes
+	var s,t; // Sommet Source - Sommet Destination (Target)
+	var bool; // Test si on choisit de mettre l’arête ou pas
+	var ind; // Indice dans le tableau du/des Sommet(s) à mettre
+	var is; // Indice dans le tableau du Sommet à enlever
+
+	// Tant que tous les sommets ne sont pas regroupés dans 1 ensemble
+	for(a=0 ; T.length != 1 ; a++){
 		
+		s = tabEdges[a].data.source;
+		t = tabEdges[a].data.target;
+		
+		// On trouve l’indice ou est le sommet à enlever
+		for(c=0 ; c < T.length ; c++){
+			if(contains(T[c],t)){
+				is = c;
+			}
+		}
+
+		// Tester si les 2 sommets ne sont pas dans le même tableau (le même ensemble)
+		bool = true;
+
+		for(b=0 ; b < T.length && bool; b++){
+			
+			// Si les 2 sommets sont dans le même ensemble
+			if(contains(T[b],s) && contains(T[b],t)){
+				bool = false;
+			}
+		}
+		
+		// On ajoute cette arête
+		if(bool == true){
+
+			tabEdgesKruskal[a] = tabEdges[a];
+			
+			ind = T[s-1].length;
+
+			for(j=0 ; j < T[is].length ; j++){
+				T[s-1][ind] = T[is][j];
+				ind++;
+			}
+			
+			T.splice(is,1);	// On supprime la ligne du tableau qui contenait les sommets déplacés
+		}
+		
+
 	}
 
-	console.log("-----");
-	console.log(tabEdges);
+	console.log('\n5 /================================================');
+	console.log('TABEDGES APRES KRUSKAL\n');
+	console.log(tabEdgesKruskal);
+
+	
 	return null;
 }
 
