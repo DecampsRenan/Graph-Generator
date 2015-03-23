39c9e4bcfb0a95c509cddc81aebf7e799f3a8fb0
Renan Decamps <renan.decamps@gmail.com>
2015-03-23 12:00:46 -0500

    Rajout du portail.js (renommé en main.js)

---
+++ portail/main.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+	cyto    = require('cytoscape'),
+	path    = require('path'),
+	app     = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function(req,res){
+	res.render('index.ejs', { });
+})
+.listen(80, function() {
+	var port = this.address().port;
+
+	console.log('App listening at http://127.0.0.1:%s', port);
+});
