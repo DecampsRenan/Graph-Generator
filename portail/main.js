fd9afb2d24a7922ebd6714acb3267edae37f093b
bvince <vi.berniere@gmail.com>
2015-03-24 15:49:19 -0400

    Revert "Affichage SSSP portail"
    This reverts commit 550ff0eea85b49fc8ec81e3da421f29f45b52834.

--- portail/main.js
+++ portail/main.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', function(req,res){
 	res.render('index.ejs', { });
 })
-.listen(81, function() {
+.listen(80, function() {
 	var port = this.address().port;
 
 	console.log('App listening at http://127.0.0.1:%s', port);
